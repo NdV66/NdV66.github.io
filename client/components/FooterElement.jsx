1637babff492c12ddce4d2e1462be03fb677035f
Marta Zażlak <marta.zazlak@hotmail.com>
2018-06-16 18:48:16 +0200

    deploy rwd

--- client/components/FooterElement.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {Container, Row, Col} from 'reactstrap';
-
-export const FooterElement = () =>
-    <Container fluid={true}>
-        <Row>
-            <Col md='12'>Narin (NdV66)</Col>
-        </Row>
-    </Container>;
