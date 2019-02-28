98ea76ffb84b885fd31ec330b25d9084e7136380
Marta Zażlak <marta.zazlak@hotmail.com>
2019-02-28 18:24:55 +0100

    add new project to panel

--- client/components/FooterElement.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {Container, Row, Col} from 'reactstrap';
-
-export const FooterElement = () =>
-    <Container fluid={true}>
-        <Row>
-            <Col md='12'>narindevain@gmail.com</Col>
-        </Row>
-    </Container>;
